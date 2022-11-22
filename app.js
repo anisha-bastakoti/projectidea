77416a7dab7e81bc6fe444abbaf4231317c4ef17
aneshabastakoti <aneshabastakoti@gmail.com>
2022-11-22 21:58:00 +0545

    first commit

---
+++ app.js
@@ -0,0 +1,35 @@
+const express =require('express');
+const morgan =require('morgan');
+const app =express();
+const path =require('path');
+const debug=require('debug');
+const nodemon =require('nodemon');
+const exp = require('constants');
+
+
+app.use(express.static('public'))//sabi kura publuc bata line ho 
+app.use('/', express.static(__dirname + "/public/" + '/images'));
+app.set('view engine','ejs');
+//ejs set garna ko lagi 
+
+//path join gareko 
+app.use('/js',express.static(path.join(__dirname+"node_modules/bootstrap/dis/js")));
+app.use('/scs',express.static(path.join(__dirname +"/node_modules/bootstrap/dist/scs")));
+
+//morgan use gareko 
+app.use(morgan('tiny'));//mid-ware
+app.get('/',function(req,res){
+            res.render('homepage') 
+        })
+//for routes
+const homeRoute= require('./routes/homepage');
+app.use('/homepage',homeRoute);
+
+
+
+
+//to run port 
+const PORT= process.env.PORT||3000
+app.listen(PORT, function(){
+    console.log("the  server start at "+PORT);
+})
---
+++ public/assets/js/main.js
@@ -0,0 +1,117 @@
+<form class="d-flex" role="search">
+            <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+            <button class="btn btn-outline-success" type="submit">Search</button>
+          </form>
+          <p>
+       Clothe cruise is a contemporary clothing brand known for its 
+      trend-driven styles with affordable prices. 
+      Drawing inspiration from the latest trends,
+       from street style to runway,clothe cruise
+       brand offers an array of styles that is fit 
+       for the fashion loving girl. From workwear to 
+       street style, night out, About Us label can keep
+        you going from day-to-night. Shop the latest 
+        collection from  clothe cruise line, ranging 
+        in dresses to tops, 
+      sweater knits, rompers, pants and outerwear.
+     </p>
+     bg-success p-2 text-dark bg-opacity-25
+     <button
+        class="navbar-toggler"
+        type="button"
+        data-bs-toggle="collapse"
+        data-bs-target="#navbarNav"
+        aria-controls="navbarNav"
+        aria-expanded="false"
+        aria-label="Toggle navigation"
+      >
+        <span class="navbar-toggler-icon"></span>
+      </button>
+
+      <div class="collapse navbar-collapse" id="navbarNav">
+        <div class="mx-auto"></div>
+        <ul class="navbar-nav">
+          <li class="nav-item">
+            <a class="nav-link text-white" href="#">Home</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link text-white" href="#">About</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link text-white" href="#">Blog</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link text-white" href="#">Pricing</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link text-white" href="#">Contact</a>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </nav>
+
+  <!-- Banner Image  -->
+  <div
+    class="banner-image w-100 vh-100 d-flex justify-content-center align-items-center"
+  >
+    <div class="content text-center">
+      <h1 class="text-white">WEB ZONE</h1>
+    </div>
+  </div>
+
+  <!-- Main Content Area -->
+  <div class="container my-5 d-grid gap-5">
+    <div class="p-5 border">
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit.
+        Necessitatibus veniam ipsa earum quibusdam, atque ipsum error maiores
+        natus iusto fugit id saepe neque rerum magni laudantium accusantium
+        dolorem numquam quasi.
+      </p>
+    </div>
+    <div class="p-5 border">
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit.
+        Necessitatibus veniam ipsa earum quibusdam, atque ipsum error maiores
+        natus iusto fugit id saepe neque rerum magni laudantium accusantium
+        dolorem numquam quasi.
+      </p>
+    </div>
+    <div class="p-5 border">
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit.
+        Necessitatibus veniam ipsa earum quibusdam, atque ipsum error maiores
+        natus iusto fugit id saepe neque rerum magni laudantium accusantium
+        dolorem numquam quasi.
+      </p>
+    </div>
+    <div class="p-5 border">
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit.
+        Necessitatibus veniam ipsa earum quibusdam, atque ipsum error maiores
+        natus iusto fugit id saepe neque rerum magni laudantium accusantium
+        dolorem numquam quasi.
+      </p>
+    </div>
+    <div class="p-5 border">
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit.
+        Necessitatibus veniam ipsa earum quibusdam, atque ipsum error maiores
+        natus iusto fugit id saepe neque rerum magni laudantium accusantium
+        dolorem numquam quasi. df
+      </p>
+    </div>
+  </div>
+  <script src="js/bootstrap.bundle.min.js"></script>
+  <script type="text/javascript">
+    var nav = document.querySelector('nav');
+
+    window.addEventListener('scroll', function () {
+      if (window.pageYOffset > 100) {
+        nav.classList.add('bg-dark', 'shadow');
+      } else {
+        nav.classList.remove('bg-dark', 'shadow');
+      }
+    });
+  </script>
